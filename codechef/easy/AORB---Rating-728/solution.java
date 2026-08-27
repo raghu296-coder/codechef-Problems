41a04d6236171dcaf2acc4e239579660c5a4fbb2
raghu296-coder <24b11ds218@adityauniversity.in>
2026-08-27 21:27:04 +0530

    ✅ AORB - Rating 728 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-27

---
+++ codechef/easy/AORB---Rating-728/solution.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for(int i=0;i<t;i++){
+		    int x = sc.nextInt();
+		    int y = sc.nextInt();
+		    int k = (500-(2*x)) + (1000-(4*(x+y)));
+		    int l = (500-(2*(x+y))) + (1000-(4*y));
+		    if(k>=l){
+		        System.out.println(k);
+		    }else{
+		        System.out.println(l);
+		    }
+		}
+
+	}
+}
