352912a54cb0c56ade65638115fb38d0fa9e993c
raghu296-coder <24b11ds218@adityauniversity.in>
2026-08-28 21:45:19 +0530

    ✅ FLOW017 - Rating 730 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-28

---
+++ codechef/easy/FLOW017---Rating-730/solution.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc =new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-- > 0){
+		    int x= sc.nextInt();
+		    int y= sc.nextInt();
+		    int z= sc.nextInt();
+		    if (x>=y && x>=z){
+		        System.out.println(Math.max(y,z));
+		    }
+		    
+		     else if(y>=x && y >=z){
+		        System.out.println(Math.max(x,z));
+		    }else {
+		        System.out.println( Math.max(x,y));
+	       	}
+		    
+    
+	}
+	sc.close();
+}
+}
