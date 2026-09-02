8cde4779840dafd41e86031e8472c2101a587bed
raghu296-coder <24b11ds218@adityauniversity.in>
2026-09-02 21:46:44 +0530

    ✅ DECINC - Rating 722 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-02

---
+++ codechef/easy/DECINC---Rating-722/solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int n= sc.nextInt;
+		if (n%4==0){
+		   n++;
+		    System.out.println(n);
+		}else{
+		    n--;
+		    System.out.println(n);
+		}
+
+	}
+}
