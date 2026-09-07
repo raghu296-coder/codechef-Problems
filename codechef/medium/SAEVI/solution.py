e061e49ffbb19207b4293f36fdfa3448ddb80a00
raghu296-coder <24b11ds218@adityauniversity.in>
2026-09-07 19:34:55 +0530

    ✅ SAEVI | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-07

--- codechef/medium/SAEVI/solution.py
+++ codechef/medium/SAEVI/solution.py
@@ -1,10 +1,11 @@
 # cook your dish here
 n,k=map(int,input().split())
 arr=list(map(int,input().split()))
-if n>2*k:
-    total=0
-    for i in range(0,n,2):
-        total+= arr[i]
-    print(total)
-else:
-    print(0)
+sum=0
+for i in range(0,n,2):
+    if(arr[i]>2*k):
+        
+        sum= sum+arr[i]
+    else:
+        return 0
+print(sum)
