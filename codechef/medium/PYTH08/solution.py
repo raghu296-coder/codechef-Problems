c59d7bb0c873969791175bca50b37ca88ba3d97d
raghu296-coder <24b11ds218@adityauniversity.in>
2026-09-11 12:28:07 +0530

    ✅ PYTH08 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-11

--- codechef/medium/PYTH08/solution.py
+++ codechef/medium/PYTH08/solution.py
@@ -1,4 +1,7 @@
-# Update the '_' in the code below to provide the following output
-# 7 3
+# Update the '_' in the code below to output the following 
+# 7
+# 3
 
-print(3 + 4, 2 + 1)
+print(3 + 4)
+
+print(2 + 1)
